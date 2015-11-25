9f018f216b3a30aa4f0ca190e269ca451276d060
Emanuele Tonello <emanueletonello@gmail.com>
2015-11-25 12:27:29 +1000

    removed unused code, fixed post processing override, add comments

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: discourse-plugin-featured-image
-# about: Add Featured Image Support
-# version: 0.2
+# about: Add Featured Image to the Topic
+# version: 0.4
 # authors: Emanuele Tonello
-# url: https://github.com/
+# url: https://github.com/startsat60/discourse-plugin-featured-image
 
 enabled_site_setting :featured_enabled
 
@@ -11,37 +11,39 @@ register_asset 'stylesheets/featured.scss'
 after_initialize do
   FEATURED_FIELD_NAME = 'featured_image'
 
-  module ::FeaturedImage
+# to handle the edge case of this post processor class that even if apparently gets called only
+# on the RSpec suite, it will override the selected featured image
+  require_dependency 'cooked_post_processor'
+  CookedPostProcessor.class_eval do
     FEATURED_FIELD_NAME = 'featured_image'
-    class Engine < ::Rails::Engine
-      engine_name 'discourse-plugin-featured-image'
-      isolate_namespace FeaturedImage
-    end
+      def update_topic_image
+        if !@post.topic.custom_fields[FEATURED_FIELD_NAME].present?
+          if @post.is_first_post?
+            img = extract_images_for_topic.first
+            @post.topic.update_column(:image_url, img["src"][0...255]) if img["src"].present?
+          end
+        end
+      end
   end
 
-# use routes?
-#   FeaturedImage::Engine.routes.draw do
-#     put '/:topic_id/featured_image' => 'feautred_image#update'
-# end
-
-  require_dependency 'application_controller'
-
-  # Add a `tags` reader to the Topic model for easy reading of tags
   add_to_class :topic, :featured_image do
     result = custom_fields[FEATURED_FIELD_NAME]
   end
 
+# handling image update
   PostRevisor.track_topic_field(:featured_image) do |tc, featured_image|
-    puts 'PostRevisor'
-    tc.record_change(FEATURED_FIELD_NAME, tc.topic.custom_fields[FEATURED_FIELD_NAME], featured_image)
     tc.topic.custom_fields.update(FEATURED_FIELD_NAME => featured_image)
+    tc.topic.image_url = featured_image
+    tc.topic.save
   end
 
+# populating field on topic creation
   on(:topic_created) do |topic, params, _user|
     topic.custom_fields[FEATURED_FIELD_NAME] = params[:featured_image]
     topic.image_url = params[:featured_image]
     topic.save
   end
 
+# adding the field to the serializer, to be able to use it in the injected template
   TopicViewSerializer.attributes_from_topic(:featured_image)
 end
